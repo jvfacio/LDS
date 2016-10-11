161f19e86dd2dfe92e8b1d70c281073c3882de27
amoatiny <amoatiny@vcu.edu>
2016-10-11 17:55:50 -0500

    removed Customer and Loan objects from Payment class

--- src/main/java/com/hxwr/lds/model/Payment.java
+++ src/main/java/com/hxwr/lds/model/Payment.java
@@ -5,11 +5,6 @@
  */
 package com.hxwr.lds.model;
 
-import com.hxwr.lds.entities.Customer;
-import com.hxwr.lds.entities.Loan;
-import java.io.PrintWriter;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -17,12 +12,10 @@ import java.util.Date;
  * @author Training
  */
 public class Payment {
-    
+
     private double amount;
     private Date date;
-    private Loan loan;
-    private Customer customer;
-    
+
     /**
      * @return the amount
      */
@@ -51,33 +44,4 @@ public class Payment {
         this.date = date;
     }
 
-    /**
-     * @return the loan
-     */
-    public Loan getLoan() {
-        return loan;
-    }
-
-    /**
-     * @param loan the loan to set
-     */
-    public void setLoan(Loan loan) {
-        this.loan = loan;
-    }
-
-    /**
-     * @return the customer
-     */
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    /**
-     * @param customer the customer to set
-     */
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-    
-    
 }
