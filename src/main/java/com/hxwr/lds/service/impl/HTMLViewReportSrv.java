0a64c814ef4c32920b90f9eb9e57736d1ee09305
Adam Curtis <kallisti.dev@gmail.com>
2016-10-13 09:52:45 -0500

    add loan view service

---
+++ src/main/java/com/hxwr/lds/service/impl/HTMLViewReportSrv.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hxwr.lds.service.impl;
+
+import com.hxwr.lds.model.LoanReport;
+import com.hxwr.lds.service.IViewReportSrv;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Training
+ */
+public class HTMLViewReportSrv implements IViewReportSrv {
+    
+    @Override
+    public view(LoanReport loan, HttpServletRequest request, HttpServletResponse response) {
+        
+    }
+    
+}
