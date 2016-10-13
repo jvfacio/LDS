cfc11527b752b6234804d08a51e4b6950a4ad0a6
antonyrmrz <antony7945@gmail.com>
2016-10-13 17:12:43 -0500

    Customer dashboard

--- src/main/java/com/hxwr/lds/controller/CustomerController.java
+++ src/main/java/com/hxwr/lds/controller/CustomerController.java
@@ -5,10 +5,17 @@
  */
 package com.hxwr.lds.controller;
 
+import com.hxwr.lds.entities.Client;
+import com.hxwr.lds.service.ICustomerSrv;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 /**
  *
@@ -17,12 +24,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CustomerController{
     
-    @RequestMapping(value = "/customer", method  = RequestMethod.GET)
+    @RequestMapping(value = "/printhello", method  = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
-    
+    /***
+     * This is to display the customer dashboard
+     * @param model
+     * @param request
+     * @param response
+     * @return 
+     */
+    @RequestMapping(value = "/customer", method  = RequestMethod.GET)
+    public String customer(ModelMap model,HttpServletRequest request, HttpServletResponse response){
+        WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getSession().getServletContext());
+        ICustomerSrv custSrv = (ICustomerSrv) context.getBean("customerSrv");
+        HttpSession httpSession  = request.getSession();
+        Object rawClient = httpSession.getAttribute("client");
+        if(rawClient == null || !(rawClient instanceof Client)) {
+            return "login";
+        }
+         else {
+            Client client = custSrv.refresh((Client) rawClient);
+            httpSession.setAttribute("client", client);
+            return "customer";
+        }   
+        
+        
+       
+    }
 
         
     
