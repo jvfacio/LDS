5b727949d6b8d8c0b31a073d3c48d39ad23c2b42
amoatiny <amoatiny@vcu.edu>
2016-10-14 14:37:57 -0500

    Fixed repeating rows

--- src/main/java/com/hxwr/lds/service/impl/PDFViewReportSrv.java
+++ src/main/java/com/hxwr/lds/service/impl/PDFViewReportSrv.java
@@ -21,77 +21,83 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletOutputStream;
 
-/** A service for generating a LoanReport view.
+/**
+ * A service for generating a LoanReport view.
  *
  * @author Training
  */
 public class PDFViewReportSrv implements IViewReportSrv {
-    
-    /** Generates a view. Implementations should use t he request/response objects
-     *  to forward/redirect users to the correct resource (HTML, PDF, etc).
+
+    /**
+     * Generates a view. Implementations should use t he request/response
+     * objects to forward/redirect users to the correct resource (HTML, PDF,
+     * etc).
+     *
      * @param loan
      * @param request
-     * @param response 
+     * @param response
      */
     @Override
-    public void view(LoanReport loan, HttpServletRequest request, HttpServletResponse response){
-        
-        
+    public void view(LoanReport loan, HttpServletRequest request, HttpServletResponse response) {
+
         Document document = new Document();
-        
+
         try {
             response.setContentType("application/pdf");
             ServletOutputStream os = response.getOutputStream();
             PdfWriter.getInstance(document, os);
-            
+
             document.open();
-            
+
             document.addTitle("Loan Report");
-            
+
             //add client information pdf
-            document.add(new Paragraph("Name: " + loan.getClient().getName()+" " + loan.getClient().getlastName()));
+            document.add(new Paragraph("Name: " + loan.getClient().getName() + " " + loan.getClient().getlastName()));
             document.add(new Paragraph("Address: " + loan.getClient().getAddress()));
             document.add(new Paragraph("Phone Number: " + loan.getClient().getPhoneNumber()));
             document.add(new Paragraph("Salary: " + loan.getClient().getSalary()));
-            
+
             //add loan information to pdf
             document.add(Chunk.NEWLINE);
             document.add(new Paragraph("Loan Type: " + loan.getLoan().getLoanType()));
             document.add(new Paragraph("Loan Amount: " + loan.getLoan().getAmount()));
             document.add(new Paragraph("Loan Period: " + loan.getLoan().getLoanPeriod()));
             document.add(Chunk.NEWLINE);
+
             //create table of payment objects
-            PdfPTable table = new PdfPTable(3);
-          
+            PdfPTable table = new PdfPTable(4);
+
             //add table headers
             table.setHeaderRows(1);
+            table.addCell("Payment #");
             table.addCell("Date");
             table.addCell("Payment");
             table.addCell("Balance");
-            
+
             //add dates, payments, and balance to table
-            for(int i = 0; i < loan.getMonthPaymentLst().size(); i++){
-                
+            for (int i = 0; i < loan.getMonthPaymentLst().size(); i++) {
+
+                table.addCell(String.valueOf(i));
                 //add date to table
                 table.addCell(loan.getMonthPaymentLst().get(i).getFormattedDate());
-                
+
                 //add payment amount to table
-                table.addCell(""+loan.getMonthPaymentLst().get(i).getAmount());
-                
+                table.addCell(String.valueOf(loan.getMonthPaymentLst().get(i).getAmount()));
+
                 //add balance to table
-                table.addCell(""+loan.getMonthPaymentLst().get(i).getPrincipal());
-                
-                //add table row to pdf
-                document.add(table);
+                table.addCell(String.valueOf(loan.getMonthPaymentLst().get(i).getPrincipal()));
+
             }
-            
-            
+
+            //add table rows to pdf
+            document.add(table);
+
         } catch (IOException | DocumentException ex) {
             Logger.getLogger(PDFViewReportSrv.class.getName()).log(Level.SEVERE, null, ex);
-        }finally{
+        } finally {
             document.close();
         }
-    
+
     }
-    
+
 }
