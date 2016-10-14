df14812a79765747d162159d4efd2fabd9907a42
jvfacio <facioo@gmail.com>
2016-10-14 10:25:33 -0500

    implement login page

--- src/main/java/com/hxwr/lds/controller/CustomerController.java
+++ src/main/java/com/hxwr/lds/controller/CustomerController.java
@@ -5,6 +5,10 @@
  */
 package com.hxwr.lds.controller;
 
+import com.hxwr.lds.entities.Client;
+import com.hxwr.lds.service.ICustomerSrv;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,10 +21,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class CustomerController{
     
+    @Autowired
+    ICustomerSrv customerSrv;
+    
     @RequestMapping(value = "/customer", method  = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
+    }
+    
+    @RequestMapping(value = "/customer/login", method  = RequestMethod.GET)
+    public String loginCustomer(ModelMap model) { 
+        return "login";
+    }
+    
+    @RequestMapping(value = "/customer/login", method  = RequestMethod.POST)
+    public String submitLogin(
+            ModelMap model,
+            String nickname, String password,
+            HttpSession session) {
+        Client client = customerSrv.validateCustomer(nickname, password);
+        if (client != null) {
+            session.setAttribute("client", client);
+            return "customer";
+        }
+        else {
+            model.addAttribute("message", "Login credentials invalid");
+            return "login";
+        }
     }
     
 
