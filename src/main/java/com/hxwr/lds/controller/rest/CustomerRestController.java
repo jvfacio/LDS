f4a3712fa1ec672c1d164a994439e60cc0fa1800
Adam Curtis <kallisti.dev@gmail.com>
2016-10-18 12:33:59 -0500

    move CustomerRestController to rest sub-package

--- src/main/java/com/hxwr/lds/controller/rest/CustomerRestController.java
+++ src/main/java/com/hxwr/lds/controller/rest/CustomerRestController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.hxwr.lds.controller;
+package com.hxwr.lds.controller.rest;
 
 import com.hxwr.lds.entities.Client;
 import com.hxwr.lds.service.ICustomerSrv;
