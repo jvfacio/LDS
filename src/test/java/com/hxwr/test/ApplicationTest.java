8b1fe5034ba035649e23910e36b9c7b8093f745a
antonyrmrz <antony7945@gmail.com>
2016-10-19 12:53:58 -0500

    changes to test clients

--- src/test/java/com/hxwr/test/ApplicationTest.java
+++ src/test/java/com/hxwr/test/ApplicationTest.java
@@ -13,6 +13,11 @@ import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import javax.ws.rs.core.MediaType;
+import io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.expect;
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
 /**
  * 
  * @author AntonyRmrz
@@ -25,16 +30,20 @@ public class ApplicationTest {
         
        //Set up new test data
     	client = new Client("Hector","844222112", "Colosio HXWR", "100000","Medina", "h1", "xxx");
-        
-        
         System.out.println("hello form init");
     }
     @Test
     public void testUnit1(){//Post a contact and retrieve it
+//        expect().
+//        statusCode(200).
+//        when().
+//                get("http://localhost:8080/LDS/rest/clients");
+//        
         System.out.println("hello from test1");
     }
     
     @Test
     public void testUnit2(){
+        System.out.println("Hello from test2");
     }
 }
