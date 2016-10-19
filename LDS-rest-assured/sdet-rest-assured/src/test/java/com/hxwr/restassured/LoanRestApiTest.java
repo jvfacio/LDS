513e0e46c64b5006d7fe42ec5b89e450a71dde2d
jvfacio <facioo@gmail.com>
2016-10-19 16:44:48 -0500

    create the restassure project

---
+++ LDS-rest-assured/sdet-rest-assured/src/test/java/com/hxwr/restassured/LoanRestApiTest.java
@@ -0,0 +1,47 @@
+package com.hxwr.restassured;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoanRestApiTest {
+	
+	/*******************************************************
+	 * Send a GET request to
+	 * http://localhost:8080/contact/1
+	 * Use Basic authentication
+	 ******************************************************/
+	
+	@Test
+	public void retrieveContact() {
+		
+		given().
+			params("grant_type","client_credentials").
+			auth().
+			preemptive().
+			basic("wsuser","t0ps3cr3t").
+		when().
+			get("http://192.168.10.50:8181/contact/1").
+		then().body("id",equalTo(1))
+                        .and().body("firstName", equalTo("Javier"))
+                        .and().body("lastName", equalTo("Mercado"))
+                        .statusCode(200);
+	}
+        
+        @Test
+	public void reviewRetrieveContactService() {
+		
+		given().
+			params("grant_type","client_credentials").
+			auth().
+			preemptive().
+			basic("wsuser","t0ps3cr3t").
+		when().
+			get("http://localhost:8181/contact/1").
+		then().statusCode(200);
+	}
+}
