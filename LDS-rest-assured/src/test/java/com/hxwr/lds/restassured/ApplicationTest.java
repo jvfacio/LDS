6da05b5c32a8c3c7d8d964c6cdc6edc566ac0e35
Adam Curtis <kallisti.dev@gmail.com>
2016-10-20 09:06:52 -0500

    move ApplicationTest to new project

--- LDS-rest-assured/src/test/java/com/hxwr/lds/restassured/ApplicationTest.java
+++ LDS-rest-assured/src/test/java/com/hxwr/lds/restassured/ApplicationTest.java
@@ -1,10 +1,11 @@
+package com.hxwr.lds.restassured;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package com.hxwr.test;
 
 
 import com.hxwr.lds.entities.Client;
