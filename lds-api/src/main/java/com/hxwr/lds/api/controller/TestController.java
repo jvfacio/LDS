6e85b22b49f23f4aeafaffd2e8bf0e8fec9c37ec
ramiro <ramiroherreraruiz@gmail.com>
2016-11-02 19:33:29 -0400

    ejemplo ramiro
    ejemplo de implementar una interfaz

--- lds-api/src/main/java/com/hxwr/lds/api/controller/TestController.java
+++ lds-api/src/main/java/com/hxwr/lds/api/controller/TestController.java
@@ -6,6 +6,7 @@
 package com.hxwr.lds.api.controller;
 
 import com.hxwr.lds.api.service.ITestService;
+import com.hxwr.lds.api.service.ITestServiceRamirin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,10 +31,17 @@ public class TestController {
     }
     
     
+     @Autowired
+    private ITestServiceRamirin testServiceRamirin;
+    
     @GetMapping("/test/{name}")
     public String xxxxzyyasosdfhk(@PathVariable("name") String name){
         
         return testService.sayHola(name);
     }
     
+    @GetMapping("/test/ramirin/{number1}/{number2}")
+    public String sayHola2(@PathVariable("number1") int number1,@PathVariable("number2") int number2 ){
+        return testServiceRamirin.add(number1,number2);
+    }
 }
---
+++ lds-api/src/main/java/com/hxwr/lds/api/service/impl/TestServiceRamirinImpl.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hxwr.lds.api.service.impl;
+
+import com.hxwr.lds.api.service.ITestServiceRamirin;
+
+/**
+ *
+ * @author ramirin
+ */
+public class TestServiceRamirinImpl implements ITestServiceRamirin{
+    
+    @Override
+    public String add(int a, int b) {
+  
+        
+    return "the addition is --> " +(a+b) ;
+    
+    }
+    
+}
