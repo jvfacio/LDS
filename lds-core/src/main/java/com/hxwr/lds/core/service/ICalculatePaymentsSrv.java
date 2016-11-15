83ec421c9ef359fefac761c200491898ec267a1a
amoatiny <amoatiny@vcu.edu>
2016-11-15 16:42:48 -0600

    Interface for the Loan Payments Calculations

---
+++ lds-core/src/main/java/com/hxwr/lds/core/service/ICalculatePaymentsSrv.java
@@ -0,0 +1,29 @@
+package com.hxwr.lds.core.service;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import com.hxwr.lds.core.entities.Client;
+import com.hxwr.lds.core.entities.Loan;
+import com.hxwr.lds.core.entities.PaymentDetail;
+import com.hxwr.lds.core.model.LoanReport;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 35194
+ */
+public interface ICalculatePaymentsSrv {
+    
+    /**
+     * Generate the loan report object
+     * @param loan loan data
+     * @return a loan report data
+     */
+    
+    public ArrayList<PaymentDetail> CalculatePayments(Loan loan);
+}
