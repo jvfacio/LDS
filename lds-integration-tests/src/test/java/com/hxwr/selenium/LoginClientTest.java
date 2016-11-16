730ac4726ec5bcb67ae42b4e243295f7b568cab8
shankarravi36 <shankece@gmail.com>
2016-11-16 11:57:07 -0600

    selenium test for login

--- lds-integration-tests/src/test/java/com/hxwr/selenium/LoginClientTest.java
+++ lds-integration-tests/src/test/java/com/hxwr/selenium/LoginClientTest.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.hxwr.selenium;
-
+import com.hxwr.selenium.AbstractSeleniumTest;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -13,4 +16,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * @author Training
  */
 public class LoginClientTest extends AbstractSeleniumTest {
+    
+    @Test
+    public void testHappyPath(){
+        String url="http://localhost:8080/lds-web-app";
+        driver.get(url);
+        driver.navigate().to("http://http://localhost:8080/lds-web-app/customer/login");
+        String title= driver.getTitle();
+        assert(title.equals("Login"));
+        driver.findElement(By.id("custNickname")).sendKeys("ok");
+        driver.findElement(By.id("custPassword")).sendKeys("ok");
+        driver.findElement(By.className("btn btn-default")).click();
+        String custTitle=driver.getTitle();
+        assert(custTitle.equals("Customer Details"));
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.findElement(By.id("navLogoutLink")).click();
+        driver.close();
+    }
+    
+    @Test
+    public void testNickNamePasswordEmpty(){
+        String url="http://localhost:8080/lds-web-app";
+        driver.get(url);
+        driver.navigate().to("http://http://localhost:8080/lds-web-app/customer/login");
+        String title= driver.getTitle();
+        assert(title.equals("Login"));
+        String nickName= driver.findElement(By.id("custNickname")).getAttribute("value");
+        String passWord= driver.findElement(By.id("custPassword")).getAttribute("value");
+        if(nickName.isEmpty()&& passWord.isEmpty()){
+             driver.findElement(By.className("btn btn-default")).click();
+             driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+             String titleTest= driver.getTitle();
+             assert(titleTest.equals("Login"));
+        }
+        driver.close();
+        
+        
+    }
 }
