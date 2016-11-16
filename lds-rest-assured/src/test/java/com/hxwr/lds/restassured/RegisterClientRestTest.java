5f6987ecf466f9dbfef8a533e900037c4712c389
antonyrmrz <antony7945@gmail.com>
2016-11-16 13:34:53 -0600

    rest assured changes with the pom file

--- lds-rest-assured/src/test/java/com/hxwr/lds/restassured/RegisterClientRestTest.java
+++ lds-rest-assured/src/test/java/com/hxwr/lds/restassured/RegisterClientRestTest.java
@@ -5,42 +5,54 @@
  */
 package com.hxwr.lds.restassured;
 
+import com.hxwr.lds.core.entities.Client;
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.expect;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
 import io.restassured.response.Response;
 import static org.hamcrest.Matchers.equalTo;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.testng.annotations.Test;
-
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Random;
 
 /**
  *
  * @author Training
  */
 public class RegisterClientRestTest {
-    private static Response response;
-    private static String jsonAsString;
+    Response response;
+    String jsonAsString;
+    ObjectMapper mapper;
+    String user;
+   
+    
     @BeforeClass
-    public void init(){
+    public static void init(){
          RestAssured.baseURI = "http://localhost:8080";
-         RestAssured.basePath = "/lds-api/client";
+         RestAssured.basePath = "";
+         //Set up new test data
+        
+        System.out.println("hello form init");
     }
     
     
     
     @Test
-	public void testUnit1() {
-		
-		given().
-			
-		when().
-			get("/getclientbynickname/ok").
-		then().body("id",equalTo(1))
-                        .and().body("name", equalTo("ddd"))
-                        .and().body("phoneNumber", equalTo("5555"))
-                        .and().body("lastName",equalTo("dddd"))
-                        .statusCode(200);
+	public void testUnit1() throws JsonProcessingException {
+        mapper = new ObjectMapper();    
+        String user= "RestUser"+new Random().nextInt(5000)+1;
+	Client client =new Client("Hector","844222112", "Colosio HXWR", "100000","Medina",user, "xxx");
+        jsonAsString = mapper.writeValueAsString(client); 
+        System.out.println(jsonAsString);
+        expect().statusCode(200)
+                    .given().contentType("application/json").body(jsonAsString)
+                    .when().post("/lds-api/client");
 	}
     @Test
         public void testUnit2(){
