ad1d56c943ebebbdfadd0b2c1748d27752e24c84
Adam Curtis <kallisti.dev@gmail.com>
2016-11-16 10:51:58 -0600

    add print statement to check webdriver.chrome.driver

--- lds-integration-tests/src/test/java/com/hxwr/selenium/AbstractSeleniumTest.java
+++ lds-integration-tests/src/test/java/com/hxwr/selenium/AbstractSeleniumTest.java
@@ -25,6 +25,7 @@ public abstract class AbstractSeleniumTest {
     @Before
     public void initDriver() {
        String temp=((System.getProperty("os.name").contains("Win"))? "C:\\chromedriver.exe":"/bin/chromedriver");
+       System.out.println(System.getProperty("webdriver.chrome.driver"));
        if(System.getProperty("webdriver.chrome.driver") == null) {
             System.setProperty("webdriver.chrome.driver",temp);
        }
