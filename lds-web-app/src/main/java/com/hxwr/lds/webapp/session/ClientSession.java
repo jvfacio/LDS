aa7b9a0b134f13876d61629c72bff3a07d3568f0
Adam Curtis <kallisti.dev@gmail.com>
2016-11-17 14:05:25 -0600

    refresh client object everytime ClientSession.getClient is called

--- lds-web-app/src/main/java/com/hxwr/lds/webapp/session/ClientSession.java
+++ lds-web-app/src/main/java/com/hxwr/lds/webapp/session/ClientSession.java
@@ -6,7 +6,10 @@
 package com.hxwr.lds.webapp.session;
 
 import com.hxwr.lds.core.entities.Client;
+import com.hxwr.lds.core.service.ICustomerSrv;
+import java.io.IOException;
 import java.io.Serializable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
@@ -19,13 +22,18 @@ import org.springframework.stereotype.Component;
 @Scope(value="session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class ClientSession implements Serializable {
   
+    @Autowired
+    private ICustomerSrv customerSrv;
+    
     private Client client = null;
     
     public boolean isLoggedIn() {
         return client != null;
     }
     
-    public Client getClient() {
+    public Client getClient() throws IOException {
+        if(client != null)
+            client = customerSrv.getCustomer(client.getId());
         return client;
     }
     
