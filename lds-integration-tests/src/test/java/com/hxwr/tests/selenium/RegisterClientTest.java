8365a37c22ab654dde6118e8fd7c5909e57e2b5d
shankarravi36 <shankece@gmail.com>
2016-11-17 17:29:01 -0600

    RegisterClient Cucumber test

--- lds-integration-tests/src/test/java/com/hxwr/tests/selenium/RegisterClientTest.java
+++ lds-integration-tests/src/test/java/com/hxwr/tests/selenium/RegisterClientTest.java
@@ -5,11 +5,18 @@
  */
 package com.hxwr.tests.selenium;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 /**
  *
@@ -20,49 +27,94 @@ public class RegisterClientTest extends AbstractSeleniumTest {
      * Test case to check the happy path
      * @param driver 
      */
-    @Test
-    public void testCase1() throws InterruptedException{
-        String url="http://localhost:8080/lds-web-app/";
-        driver.get(url);
-        String user="u"+Integer.toString(new Random().nextInt(50000)+1);
-        driver.findElement(By.id("navRegisterUrl")).click();
-        driver.findElement(By.id("nickName")).sendKeys(user);
-        driver.findElement(By.id("pass")).sendKeys("12345");
-        driver.findElement(By.id("name")).sendKeys("Antony");
-        driver.findElement(By.id("lastName")).sendKeys("Ramirez");
-        driver.findElement(By.id("address")).sendKeys("ahdiasuhdiuahsiuh");
-        driver.findElement(By.id("phoneNumber")).sendKeys("8887778891");
-        driver.findElement(By.id("salary")).sendKeys("100000");
-        driver.findElement(By.id("submitUser")).click();
-        driver.findElement(By.id("navLogoutLink")).click();
-        Thread.sleep(1000);
-        driver.close();
-    }
-    /***
-     * Test case to know if the web program accepts negative values 
-     * @param driver 
-     */
-    @Test
-    public void testCase2() throws InterruptedException{ 
-        String url="http://localhost:8080/lds-web-app/";
-        driver.get(url);
-        //random user
-        String user="un"+Integer.toString(new Random().nextInt(50000)+1);
-        driver.findElement(By.id("navRegisterUrl")).click();
-        driver.findElement(By.id("nickName")).sendKeys(user);
-        assert(driver.findElement(By.id("nickName")).getAttribute("value").equals(user));
-        driver.findElement(By.id("pass")).sendKeys("12345");
-        driver.findElement(By.id("name")).sendKeys("Antony");
-        driver.findElement(By.id("lastName")).sendKeys("Ramirez");
-        driver.findElement(By.id("address")).sendKeys("ahdiasuhdiuahsiuh");
-        driver.findElement(By.id("phoneNumber")).sendKeys("8887778891");
-        driver.findElement(By.id("salary")).sendKeys("-100000");
-        assert(driver.findElement(By.id("salary")).getAttribute("value").equals("-100000"));
-        driver.findElement(By.id("submitUser")).click();
-        Thread.sleep(1000);
-        driver.close();
+    
+    private List<String> nickName;
+    private List<String> passWord;
+    private List<String> firstName;
+    private List<String> lastName;
+    private List<String> address;
+    private List<String> phoneNumber;
+    private List<Integer> salary;
+    
+    @Given("^the LDS homepage for the loan delivery system$") 
+    public void goToHome(){
+        driver.get("http://localhost:8080/lds-web-app/");
     }
     
-    
-}
-
+    @When("^I navigate to the register page$")
+    public void navigate(){
+        driver.navigate().to("http://localhost:8080/lds-web-app/customer/register");
+    }
+    @When("^I enter Nickname with$")
+    public void enterNickName(List<String> nickName){
+        this.nickName=nickName;
+        for(String n:nickName){
+        driver.findElement(By.id("nickName")).sendKeys(n);
+        }
+    }
+    @When("^I enter Password with$")
+    public void enterPassword(List<String> passWord){
+        this.passWord=passWord;
+        for(String p:passWord)
+        driver.findElement(By.id("pass")).sendKeys(p);
+        }
+    @When("^I enter First Name with$")
+    public void enterFirstName(List<String> firstName){
+        this.firstName=firstName;
+        for(String f:firstName){
+        driver.findElement(By.id("name")).sendKeys(f);
+        }
+    }
+    @When("^I enter last Name with$")
+    public void enterLastName(List<String> lastName){
+        this.lastName=lastName;
+        for(String l:lastName){
+            driver.findElement(By.id("lastName")).sendKeys(l);
+        }
+    }
+    @When("^I enter Address with$")
+    public void enterAddress(List<String> address){
+        this.address=address;
+        for(String a:address){
+            driver.findElement(By.id("address")).sendKeys(a);
+        }
+     
+    }
+    @When("^I enter PhoneNumber with$")
+    public void enterPhoneNumber(List<String> phoneNumber){
+        this.phoneNumber=phoneNumber;
+        for(String ph:phoneNumber){
+            driver.findElement(By.id("phoneNumber")).sendKeys(ph);
+          }
+          
+      }
+    @When("^I enter Salary with$")
+    public void enterSalary(List<Integer> salary){
+        this.salary=salary;
+        for(Integer sal:salary){
+            driver.findElement(By.id("salary")).sendKeys(sal.toString());
+          }
+      }
+    @When("^I click Submit$")
+    public void clickSubmit(){
+            driver.findElement(By.id("submitUser")).click();
+      }
+    @Then("^I should be able to view the customer details page$")
+    public void assertTitle(){
+            String title= driver.getTitle();
+            assert(title.equals("LDS - Customer Details"));
+      }
+    @Then("^I should see Account created successfully$")
+    public void checkMessage(){
+          String message= driver.findElement(By.id("message")).getText();
+          assert(message.equals("Account successfully created."));
+          driver.close();
+      }
+    @Then("^I should not be able to register successfully$")
+    public void unableToRegister(){
+          String title=driver.getTitle();
+          assert(title.equals("LDS - Register"));
+          driver.close();
+      }
+    }
+  
