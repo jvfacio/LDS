1cdbc4fbdc45b8824f44986074d039550cddd6ce
shankarravi36 <shankece@gmail.com>
2016-11-18 18:08:40 -0600

    changing the setters and getters of client object inside loan

--- lds-web-app/src/main/java/com/hxwr/lds/webapp/service/LoanSrvImpl.java
+++ lds-web-app/src/main/java/com/hxwr/lds/webapp/service/LoanSrvImpl.java
@@ -9,6 +9,7 @@ package com.hxwr.lds.webapp.service;
 
 import com.hxwr.lds.core.service.ILoanSrv;
 import com.hxwr.lds.core.entities.Loan;
+import com.hxwr.lds.core.service.ICustomerSrv;
 import com.hxwr.lds.webapp.IRestClient;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,9 @@ public class LoanSrvImpl implements ILoanSrv {
     @Autowired 
     IRestClient restClient;
     
+    @Autowired
+    ICustomerSrv customerSrv;
+    
     @Override
      public Loan fetchLoanByID(int loanID)throws IOException{
          return restClient.makeRequest("GET","/loan/" + loanID, Loan.class);
@@ -30,9 +34,10 @@ public class LoanSrvImpl implements ILoanSrv {
      
      @Override
      public void addLoanDetails(Loan loan)throws IOException {
-        Loan newLoan = restClient.makeRequest("POST", "/client/"+ loan.getClient().getId()+ "/createLoan", loan, Loan.class);
+        Loan newLoan = restClient.makeRequest("POST", "/client/"+loan.getClientId() + "/createLoan", loan, Loan.class);
         loan.setLoanID(newLoan.getLoanID());
-        loan.getClient().addLoan(loan);
+        
+        
      }
     
 }
